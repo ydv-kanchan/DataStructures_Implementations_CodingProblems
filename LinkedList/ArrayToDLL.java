198e75b5b3f9a9b1937f7a2c1c83901277872426
ydv-kanchan <ydv.kanchan24@gmail.com>
2025-12-08 12:33:01 +0530

    delete head of a doubly linkedlist

--- LinkedList/ArrayToDLL.java
+++ LinkedList/ArrayToDLL.java
@@ -33,25 +33,41 @@ public class ArrayToDLL {
     //         temp = temp.next;
     //     }
     // }
-public static void printLL(Node head) {
-    Node temp = head;
-    while (temp != null) {
-        System.out.println("Current: " + temp.data);
-        System.out.println("Prev: " + (temp.prev != null ? temp.prev.data : "null"));
-        System.out.println("Next: " + (temp.next != null ? temp.next.data : "null"));
-        System.out.println("-------------------");
-        temp = temp.next;
+    public static void printLL(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println("Current: " + temp.data);
+            System.out.println("Prev: " + (temp.prev != null ? temp.prev.data : "null"));
+            System.out.println("Next: " + (temp.next != null ? temp.next.data : "null"));
+            System.out.println("-------------------");
+            temp = temp.next;
+        }
     }
-}
+    
+    public static Node deleteHeadDLL(Node head) {
+        if (head == null || head.next == null)
+            return null;
+        head = head.next;
+        head.prev = null;
+        return head;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        // int n = sc.nextInt();
+        // int[] arr = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     arr[i] = sc.nextInt();
+        // }
+        int m = sc.nextInt();
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = sc.nextInt();
         }
-        printLL(arraytoDLL(arr));
+        Node head = arraytoDLL(arr2);
+        printLL(head);
+        head = deleteHeadDLL(head);
+        printLL(head);
         sc.close();
     }
 }
